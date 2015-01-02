9003eec2007ac51fb08203315876c914f0f61d8a
Kevin Lohman <badpirate@gmail.com>
2015-01-02 01:07:59 -0800

    SWIFT!  URL Encode handling for strings and parameter lists.  Similar to NSString+BP

---
+++ String+BP.swift
@@ -0,0 +1,40 @@
+//
+//  NSString+BP.swift
+//  Node War
+//
+//  Created by Kevin Lohman on 1/1/15.
+//  Copyright (c) 2015 Logic High. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func URLEncodedString() -> String? {
+        var customAllowedSet =  NSCharacterSet.URLQueryAllowedCharacterSet()
+        var escapedString = self.stringByAddingPercentEncodingWithAllowedCharacters(customAllowedSet)
+        return escapedString
+    }
+    static func queryStringFromParameters(parameters: Dictionary<String,String>) -> String? {
+        if (parameters.count == 0)
+        {
+            return nil
+        }
+        var queryString : String? = nil
+        for (key, value) in parameters {
+            if let encodedKey = key.URLEncodedString() {
+                if let encodedValue = value.URLEncodedString() {
+                    if queryString == nil
+                    {
+                        queryString = "?"
+                    }
+                    else
+                    {
+                        queryString! += "&"
+                    }
+                    queryString! += encodedKey + "=" + encodedValue
+                }
+            }
+        }
+        return queryString
+    }
+}
